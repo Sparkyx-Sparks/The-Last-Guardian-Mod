60bdd27ecdb09db59a1ed33f687029c560bdbeff
Sparkyx-Sparks <33848829+Sparkyx-Sparks@users.noreply.github.com>
2017-11-22 22:11:44 +0100

    Add files via upload

--- main/java/sparkyx_sparks/mod/init/BlockInit.java
+++ main/java/sparkyx_sparks/mod/init/BlockInit.java
@@ -15,5 +15,8 @@ public class BlockInit {
 	
 	public static final Block TILES_STONE = new BlockBase("stone_tiles", Material.ROCK);
 	public static final Block STRIPED_STONE = new BlockBase("stone_striped", Material.ROCK);
+	public static final Block STONE_FLOOR_SLAB = new BlockBase("stone_floor_slab", Material.ROCK);
+	public static final Block STONE_MOSS = new BlockBase("stone_moss", Material.ROCK);
+	public static final Block ROCKS_MOSS = new BlockBase("rocks_moss", Material.ROCK);
 
 }
